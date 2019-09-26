324cab94947f5329e09f51b93a9e90365b4140fa
Aldiantoro Nugroho <kriwil@gmail.com>
2019-09-26 20:07:49 +0700

    add timestamp and parallel

--- reset_pace.sh
+++ reset_pace.sh
@@ -1,4 +1,7 @@
+start=`date`
 docker-compose exec postgres9 dropdb -U aldi pace
 docker-compose exec postgres9 createdb -U aldi pace
-docker-compose exec postgres9 pg_restore -d pace -U aldi -O --clean /data/db.dump
-docker-compose exec postgres9 pg_restore -d pace -U aldi -O --clean /data/db.dump
+docker-compose exec postgres9 pg_restore -d pace -U aldi -j6 -O --clean /data/pace.dump
+docker-compose exec postgres9 pg_restore -d pace -U aldi -j6 -O --clean /data/pace.dump
+end=`date`
+echo $start -- $end
